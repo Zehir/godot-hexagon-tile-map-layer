571333b80f63a5c386bf9e6240db20d20ec9c993
Zehir <zehir@zorim.fr>
2025-03-29 12:09:51 +0100

    splitup files

---
+++ addons/hexagon_tilemaplayer/demo/demo_manager.gd
@@ -0,0 +1,7 @@
+extends Node
+
+@onready var tile_map: HexagonTileMapLayer = %HexagonTileMapLayerHorizontal
+
+
+func _ready() -> void:
+	pass
--- addons/hexagon_tilemaplayer/demo/left_menu.gd
+++ addons/hexagon_tilemaplayer/demo/left_menu.gd
@@ -1,8 +1,11 @@
-extends Node2D
+extends VBoxContainer
 
-@onready var hexagon_tile_map_layer_horizontal: HexagonTileMapLayer = $HexagonTileMapLayerHorizontal
-@onready var hexagon_tile_map_layer_vertical: HexagonTileMapLayer = $HexagonTileMapLayerVertical
-@onready var tile_map: HexagonTileMapLayer = hexagon_tile_map_layer_horizontal
+const DemoManager = preload("uid://c5t8k8u70hsgr")
+
+@onready var demo: DemoManager = %DemoManager
+
+@onready var hexagon_tile_map_layer_horizontal: HexagonTileMapLayer = %HexagonTileMapLayerHorizontal
+@onready var hexagon_tile_map_layer_vertical: HexagonTileMapLayer = %HexagonTileMapLayerVertical
 
 @onready var button_layout_vertical: CheckBox = %LayoutVertical
 @onready var button_layout_horizontal: CheckBox = %LayoutHorizontal
@@ -16,8 +19,10 @@ extends Node2D
 func _ready() -> void:
 	button_layout_horizontal.button_pressed = hexagon_tile_map_layer_horizontal.is_visible_in_tree()
 	button_layout_vertical.button_pressed = hexagon_tile_map_layer_vertical.is_visible_in_tree()
-	button_tile_set_layout_option.selected = tile_map.tile_set.tile_layout
-	button_display_pathfinding_connections.visible =  hexagon_tile_map_layer_horizontal.pathfinding_enabled
+	button_tile_set_layout_option.selected = demo.tile_map.tile_set.tile_layout
+	button_display_pathfinding_connections.visible = (
+		hexagon_tile_map_layer_horizontal.pathfinding_enabled
+	)
 	button_enable_pathfinding.button_pressed = hexagon_tile_map_layer_horizontal.pathfinding_enabled
 	button_display_pathfinding_connections.button_pressed = (
 		hexagon_tile_map_layer_horizontal.debug_mode
@@ -38,36 +43,40 @@ func _on_tile_layout_offset_horizontal_pressed() -> void:
 
 
 func switch_to_tilemap(new_tile_map: HexagonTileMapLayer):
-	tile_map.visible = false
-	var old_layout = tile_map.tile_set.tile_layout
-	tile_map = new_tile_map
-	tile_map.visible = true
-	tile_map.pathfinding_enabled = button_enable_pathfinding.button_pressed
+	demo.tile_map.visible = false
+	var old_layout = demo.tile_map.tile_set.tile_layout
+	demo.tile_map = new_tile_map
+	demo.tile_map.visible = true
+	_on_enable_pathfinding_toggled(button_enable_pathfinding.button_pressed)
 	update_debug_mode()
-
 	_on_tile_layout_selected(old_layout)
 
 
 func _on_tile_layout_selected(index: int) -> void:
-	tile_map.update_cells_layout(tile_map.tile_set.tile_layout, index as TileSet.TileLayout)
-	tile_map.tile_set.tile_layout = index as TileSet.TileLayout
-	_on_enable_pathfinding_toggled(tile_map.pathfinding_enabled)
+	demo.tile_map.update_cells_layout(
+		demo.tile_map.tile_set.tile_layout, index as TileSet.TileLayout
+	)
+	demo.tile_map.tile_set.tile_layout = index as TileSet.TileLayout
+	_on_enable_pathfinding_toggled(demo.tile_map.pathfinding_enabled)
 
 
 func _on_enable_pathfinding_toggled(toggled_on: bool) -> void:
-	tile_map.pathfinding_enabled = toggled_on
+	demo.tile_map.pathfinding_enabled = toggled_on
 	button_display_pathfinding_connections.visible = toggled_on
-	
-	if tile_map.pathfinding_enabled:
-		tile_map.astar_changed.connect(tile_map._draw_debug, CONNECT_DEFERRED + CONNECT_ONE_SHOT)
-		tile_map.pathfinding_generate_points()
+
+	if demo.tile_map.pathfinding_enabled:
+		demo.tile_map.astar_changed.connect(
+			demo.tile_map._draw_debug, CONNECT_DEFERRED + CONNECT_ONE_SHOT
+		)
+		demo.tile_map.pathfinding_generate_points()
 	else:
-		if tile_map.astar:
-			tile_map.astar = null
-		tile_map._draw_debug.call_deferred()
+		if demo.tile_map.astar:
+			demo.tile_map.astar = null
+		demo.tile_map._draw_debug.call_deferred()
+
 
 func update_debug_mode() -> void:
-	tile_map.debug_mode = (
+	demo.tile_map.debug_mode = (
 		(
 			HexagonTileMapLayer.DebugModeFlags.CONNECTIONS
 			if button_display_pathfinding_connections.button_pressed
