21307bb5b99b74201359ab316bec4f758b3748dc
Zehir <zehir@zorim.fr>
2025-03-29 11:24:09 +0100

    cleanup demo

--- addons/hexagon_tilemaplayer/demo/demo.gd
+++ addons/hexagon_tilemaplayer/demo/demo.gd
@@ -1,50 +1,44 @@
 extends Node2D
 
-const STACKED_HORIZONTAL = preload("uid://brm40hxx3wp8n")
-const STACKED_VERTICAL = preload("uid://l4anov1m1f53")
-@onready var hexagon_tile_map_layer: HexagonTileMapLayer = $HexagonTileMapLayer
+@onready var hexagon_tile_map_layer_horizontal: HexagonTileMapLayer = $HexagonTileMapLayerHorizontal
+@onready var hexagon_tile_map_layer_vertical: HexagonTileMapLayer = $HexagonTileMapLayerVertical
+@onready var tile_map: HexagonTileMapLayer = hexagon_tile_map_layer_horizontal
+
 @onready var button_layout_vertical: CheckBox = %LayoutVertical
 @onready var button_layout_horizontal: CheckBox = %LayoutHorizontal
 @onready var tile_set_layout_option: OptionButton = %TileSetLayout
 
 
 func _ready() -> void:
-	if (
-		hexagon_tile_map_layer.tile_set.tile_offset_axis
-		== TileSet.TileOffsetAxis.TILE_OFFSET_AXIS_HORIZONTAL
-	):
-		button_layout_horizontal.button_pressed = true
-	else:
-		button_layout_vertical.button_pressed = true
-
-	tile_set_layout_option.selected = hexagon_tile_map_layer.tile_set.tile_layout
+	button_layout_horizontal.button_pressed = hexagon_tile_map_layer_horizontal.is_visible_in_tree()
+	button_layout_vertical.button_pressed = hexagon_tile_map_layer_vertical.is_visible_in_tree()
+	tile_set_layout_option.selected = tile_map.tile_set.tile_layout
 
 
 func _on_tile_layout_offset_vertical_pressed() -> void:
-	hexagon_tile_map_layer.tile_set = STACKED_VERTICAL
-	hexagon_tile_map_layer.astar_changed.connect(
-		_on_tile_layout_selected.bind(tile_set_layout_option.selected), CONNECT_ONE_SHOT
-	)
-	hexagon_tile_map_layer.pathfinding_generate_points()
-
+	tile_map = hexagon_tile_map_layer_vertical
+	hexagon_tile_map_layer_horizontal.visible = false
+	hexagon_tile_map_layer_vertical.visible = true
+	_on_tile_layout_selected(hexagon_tile_map_layer_horizontal.tile_set.tile_layout)
 
 func _on_tile_layout_offset_horizontal_pressed() -> void:
-	hexagon_tile_map_layer.tile_set = STACKED_HORIZONTAL
-	hexagon_tile_map_layer.astar_changed.connect(
-		_on_tile_layout_selected.bind(tile_set_layout_option.selected), CONNECT_ONE_SHOT
-	)
-	hexagon_tile_map_layer.pathfinding_generate_points()
+	tile_map = hexagon_tile_map_layer_horizontal
+	hexagon_tile_map_layer_vertical.visible = false
+	hexagon_tile_map_layer_horizontal.visible = true
+	_on_tile_layout_selected(hexagon_tile_map_layer_vertical.tile_set.tile_layout)
 
 
 func _on_tile_layout_selected(index: int) -> void:
-	hexagon_tile_map_layer.update_cells_layout(
-		hexagon_tile_map_layer.tile_set.tile_layout, index as TileSet.TileLayout
+	tile_map.update_cells_layout(
+		tile_map.tile_set.tile_layout, index as TileSet.TileLayout
 	)
+	
+	tile_map.tile_set.tile_layout = index as TileSet.TileLayout
 
-	if hexagon_tile_map_layer.pathfinding_enabled:
-		hexagon_tile_map_layer.astar_changed.connect(
-			hexagon_tile_map_layer._draw_debug, CONNECT_DEFERRED + CONNECT_ONE_SHOT
+	if tile_map.pathfinding_enabled:
+		tile_map.astar_changed.connect(
+			tile_map._draw_debug, CONNECT_DEFERRED + CONNECT_ONE_SHOT
 		)
-		hexagon_tile_map_layer.pathfinding_generate_points()
+		tile_map.pathfinding_generate_points()
 	else:
-		hexagon_tile_map_layer._draw_debug.call_deferred()
+		tile_map._draw_debug.call_deferred()
